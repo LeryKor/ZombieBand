151c1ef56824ecccf3e6417b526848eb161efbed
LeryKor <49760813+LeryKor@users.noreply.github.com>
2019-06-15 17:59:48 +0300

    fghafgh

--- ZombieBand3D/Assets/Scripts/AiZombie.cs
+++ ZombieBand3D/Assets/Scripts/AiZombie.cs
@@ -9,31 +9,46 @@ public class AiZombie : MonoBehaviour
     public Animator animator;
     public float speed = 20.0f;
     public GameObject target;
+    private bool _alive = true;
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>(); 
+        animator = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         _distance = Vector3.Distance(target.transform.position, gameObject.transform.position);
-        if (_distance <= _minDistance)
+        if (_alive == true)
         {
-            animator.SetBool("Near", true);
-        }
-        if (_distance > _minDistance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
-            animator.SetBool("Near", false);
-            if (target.transform.position.x > transform.position.x)
+            if (_distance <= _minDistance)
             {
-                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                if (target.GetComponent<Animator>().GetBool("Strike") == true)
+                {
+                    animator.SetBool("Kill", true);
+                    _alive = false;
+                    GetComponent<CapsuleCollider2D>().enabled = false;
+                    transform.Translate(0, 2.4f, 0);
+                    GetComponent<BoxCollider2D>().enabled = true;
+                }
+                animator.SetBool("Near", true);
             }
-            else
+            if (_distance > _minDistance)
             {
-                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+                animator.SetBool("Near", false);
+                if (target.transform.position.x > transform.position.x)
+                {
+                    gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                }
+                else
+                {
+                    gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                }
+
             }
         }
     }
